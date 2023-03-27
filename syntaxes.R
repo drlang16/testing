c5a205ca542cb3e1ba8e1f1d9152bf18ee60a9b0
drlang16 <derolanguage@gmail.com>
2023-03-28 08:51:51 +0900

    ytyt

---
+++ syntaxes.R
@@ -0,0 +1,47 @@
+
+# which()
+# any()
+# all()
+
+# submit() same as source
+
+
+#args(function)
+
+
+#sd()
+#floor()
+#sum()
+#median()
+
+#evaluate(function(x){x+1}, 6)
+
+#paste()
+#paste(, sep=" ")
+
+# telegram <- function(...){
+#   paste("START", ..., "STOP")
+# }
+# telegram("goblok", "anjing", "babi")
+# telegram(c("goblok", "anjing", "babi"))
+
+
+# mad_libs <- function(...){
+#   # Do your argument unpacking here!
+#   args <- list(...)
+#   
+#   place <- args[["place"]]
+#   adjective <- args[["adjective"]]
+#   noun <- args[["noun"]]
+#   
+#   # Don't modify any code below this comment.
+#   # Notice the variables you'll need to create in order for the code below to
+#   # be functional!
+#   paste("News from", place, "today where", adjective, "students took to the streets in protest of the new", noun, "being installed on campus.")
+# }
+
+
+# "%p%" <- function(){ # Remember to add arguments!
+#   paste(left, right, sep = " ")
+# }
+# "I" %p% "love" %p% "R!"
