3bfb62629593a9975d7eb9ebc4c4bcb97ed3b482
drlang16 <derolanguage@gmail.com>
2023-03-29 02:02:26 +0900

    week 2

--- syntaxes.R
+++ syntaxes.R
@@ -1,3 +1,21 @@
+#class
+#unclass -> number of days since 1970-01-01
+
+#Sys.Date()
+#Sys.time()
+#as.POSIXlt(Sys.time())
+#str(unclass())
+#weekdays(Sys.Date())
+#months(`time or date`)
+#quarters(`time or date`)
+#t3 <- "October 17, 1986 08:24"
+#strptime(t3, "%B %d, %Y %H:%M")
+#Sys.Time() > t1
+#Sys.Time() - t1
+#difftime(Sys.time(), t1, units = 'days')
+
+#y = NULL
+#is.null(y)
 
 # which()
 # any()
@@ -29,6 +47,22 @@
 # mad_libs <- function(...){
 #   # Do your argument unpacking here!
 #   args <- list(...)
+#
+#   
+#   place <- args[["place"]] or $place
+#   adjective <- args[["adjective"]]
+#   noun <- args[["noun"]]
+#   
+#   # Don't modify any code below this comment.
+#   # Notice the variables you'll need to create in order for the code below to
+#   # be functional!
+#   paste("News from", place, "today where", adjective, "students took to the streets in protest of the new", noun, "being installed on campus.")
+# }
+
+# mad_libs <- function(...){
+#   # Do your argument unpacking here!
+#   args <- c(...)
+#
 #   
 #   place <- args[["place"]]
 #   adjective <- args[["adjective"]]
